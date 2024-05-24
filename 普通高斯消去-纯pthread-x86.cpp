b477c7db585672530c873266a384ef0a396bda6a
NEVERLAND_mercury <132367188+NEVERLANDlpx@users.noreply.github.com>
2024-05-24 16:30:03 +0800

    Add files via upload

---
+++ 普通高斯消去-纯pthread-x86.cpp
@@ -0,0 +1,144 @@
+#include<iostream>
+#include <stdio.h>
+#include<typeinfo>
+#include <stdlib.h>
+#include<semaphore.h>
+#include<pthread.h>
+#include<tmmintrin.h>
+#include<xmmintrin.h>
+#include<emmintrin.h>
+#include<pmmintrin.h>
+#include<smmintrin.h>
+#include<nmmintrin.h>
+#include<immintrin.h>
+#include<windows.h>
+using namespace std;
+#define N 1000
+
+#define NUM_THREADS 7
+float** A = NULL;
+
+
+long long head, tail, freq;
+
+sem_t sem_main;  //信号量
+sem_t sem_workstart[NUM_THREADS];
+sem_t sem_workend[NUM_THREADS];
+
+sem_t sem_leader;
+sem_t sem_Division[NUM_THREADS];
+sem_t sem_Elimination[NUM_THREADS];
+
+pthread_barrier_t barrier_Division;
+pthread_barrier_t barrier_Elimination;
+
+struct threadParam_t {    //参数数据结构
+    int k;
+    int t_id;
+};
+
+void A_init() {     //未对齐的数组的初始化
+    A = new float* [N];
+    for (int i = 0; i < N; i++) {
+        A[i] = new float[N];
+    }
+    for (int i = 0; i < N; i++) {
+        A[i][i] = 1.0;
+        for (int j = i + 1; j < N; j++) {
+            A[i][j] = rand() % 1000;
+        }
+
+    }
+    for (int k = 0; k < N; k++) {
+        for (int i = k + 1; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                A[i][j] += A[k][j];
+                A[i][j] = (int)A[i][j] % 1000;
+            }
+        }
+    }
+}
+
+void deleteA() {
+    for (int i = 0; i < N; i++) {
+        delete[] A[i];
+    }
+    delete A;
+}
+
+void print(float** a) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+
+void* LU_threadFunc(void* param) {
+    threadParam_t* p = (threadParam_t*)param;
+    int k = p->k;           //消去的轮次
+    int t_id = p->t_id;     //线程
+    int i = k + t_id + 1;   //获取任务
+
+    for (int j = k + 1; j < N; j++) {
+        A[i][j] = A[i][j] - A[i][k] * A[k][j];
+    }
+    A[i][k] = 0;
+    pthread_exit(NULL);
+    return NULL;
+}
+
+void LU_pthread_dynamic() {    //串行，动态分配线程
+    for (int k = 0; k < N; k++) {
+        for (int j = k + 1; j < N; j++) {
+            A[k][j] = A[k][j] / A[k][k];
+        }
+        A[k][k] = 1.0;
+
+        int thread_cnt = N - 1 - k;
+        pthread_t* handle = (pthread_t*)malloc(thread_cnt * sizeof(pthread_t));
+        threadParam_t* param = (threadParam_t*)malloc(thread_cnt * sizeof(threadParam_t));
+
+        for (int t_id = 0; t_id < thread_cnt; t_id++) {//分配任务
+            param[t_id].k = k;
+            param[t_id].t_id = t_id;
+        }
+
+        for (int t_id = 0; t_id < thread_cnt; t_id++) {
+            pthread_create(&handle[t_id], NULL, LU_threadFunc, &param[t_id]);
+        }
+
+        for (int t_id = 0; t_id < thread_cnt; t_id++) {
+            pthread_join(handle[t_id], NULL);
+        }
+        free(handle);
+        free(param);
+    }
+
+}
+
+
+void cal(void(*func)()) {
+    A_init();
+    QueryPerformanceCounter((LARGE_INTEGER*)&head);
+    func();
+    QueryPerformanceCounter((LARGE_INTEGER*)&tail);
+
+}
+
+int main() {
+
+    QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
+
+
+    cal(LU_pthread_dynamic);
+    cout << "平凡pthread耗时：" << (tail - head) * 1000 / freq << "ms" << endl;
+    deleteA();
+
+ 
+
+
+}
+
